fd233a3114870915df41e0f0c9a0fbb8044986ae
Jonathan0827 <limjunehyeop@gmail.com>
2024-04-07 14:41:32 +0900

    Initial Commit

---
+++ MaterialSwift/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MaterialSwift
+//
+//  Created by 임준협 on 4/7/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MaterialSwift/MaterialSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  MaterialSwiftApp.swift
+//  MaterialSwift
+//
+//  Created by 임준협 on 4/7/24.
+//
+
+import SwiftUI
+
+@main
+struct MaterialSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
